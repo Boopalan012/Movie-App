db3b7ca8ec8e981ac4834bf38e9190b438be932b
Boopalan012 <r.boopalan01@gmail.com>
2023-07-12 00:18:52 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
-import './App.css';
-import { Movie } from './Components/Movies';
+import Movies from './Components/Movies';
 
 function App() {
   return (
     <div className="App">
-      <Movie/>
+      <Movies/>
     </div>
   );
 }
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,35 +1,91 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import * as Constants from "./Constants";
-import { Card, CardMedia } from '@mui/material';
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Icon,
+  IconButton,
+  Rating,
+  Typography,
+} from "@mui/material";
+import styles from "../styles.module.css";
+import { green } from "@mui/material/colors";
 
-
-export const Movie = () => {
+const Movies = () => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const response = await axios.get(Constants.POPULAR_URL);
-        console.log(response);
         setMovies(response.data.results);
       } catch (error) {
         console.error("Error fetching movies:", error);
       }
     };
-    console.log(Constants.POPULAR_URL);
-    console.log('movies',movies);
 
     fetchMovies();
   }, []);
+  console.log("movies", movies);
 
-  return(
+  return (
     <>
-    Header
-    <Card>
-      <CardMedia title="" image="" />
+      <h1>Header</h1>
+      <div className={styles.container}>
+        {movies.map((movie) => (
+          <Card key={movie.id} className={styles.card}>
+            <CardHeader
+              className={styles.cardHeader}
+              avatar={
+                <Avatar className={styles.avatar}>{movie.vote_average}</Avatar>
+              }
+              title={
+                <span className={styles.cardHeaderTitle}>
+                  {movie.original_title}
+                </span>
+              }
+              subheader={
+                <span className={styles.cardHeaderSubheader}>
+                  {movie.release_date}
+                </span>
+              }
+              action={
+                <IconButton
+                  className={styles.iconButton}
+                  color="error"
+                  aria-label="icon button"
+                >
+                  <Icon className={styles.icon}>add_circle</Icon>
+                </IconButton>
+              }
+            />
 
-    </Card>
+            <CardMedia
+              component="img"
+              image={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              className={styles.cardImage}
+            />
+            <CardContent>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {movie.overview}
+              </Typography>
+              <Rating
+                name="movie-rating"
+                value={movie.vote_average / 2}
+                precision={0.5}
+                readOnly
+              />
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </>
-  )
+  );
 };
+
+export default Movies;
